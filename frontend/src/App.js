4c028775e1f6f50a43a9053e5da2d23fd72356b7
Minindu Madhawa <minidumadawa259@gmail.com>
2026-03-21 11:24:00 +0530

    Revert "delete home and signin ...."
    This reverts commit 39df91b80982089928fec62dc2486fdbd31a37c6.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Components/Home/Home';
+import Login from './Components/Auth/Login';
+import Signup from './Components/Auth/Signup';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     <h1>hiii</h1>
-     <h1>hiii</h1>
-     <h1>hiii</h1>
-     <h1>hiii</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
