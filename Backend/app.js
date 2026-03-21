256aa0f01700c61e9c1da70d20a36b0ba5536e1a
Minindu Madhawa <minidumadawa259@gmail.com>
2026-03-21 11:26:47 +0530

    Revert "update backend signup"
    This reverts commit 121b5db1834959da06479bb5680ed5b622da5fa5.

--- Backend/app.js
+++ Backend/app.js
@@ -1,28 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use("/", (req,res, nset) => {
+    res.send("It is working");
+})
 
-const studentRoutes = require("./Routes/studentRoutes");
-const companyRoutes = require("./Routes/companyRoutes");
-
-app.use("/api/students", studentRoutes);
-app.use("/api/companies", companyRoutes);
-
-app.get("/", (req, res) => {
-    res.send("CareerPath API is running");
-});
-
-mongoose.connect("mongodb+srv://admin:H4eQUj4fjzqgjrlX@cluster0.j4bshhl.mongodb.net/CareerPath?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://admin:H4eQUj4fjzqgjrlX@cluster0.j4bshhl.mongodb.net/")
 .then(()=> console.log("Connected to mongoDB"))
 .then(()=> {
-    app.listen(5000, () => {
-        console.log("Server is running on port 5000");
-    });
+    app.listen(5000);
 })
 .catch((err)=> console.log((err)));
